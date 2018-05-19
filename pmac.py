0f43365d2514aadc3ec28a6dcd581915743de348
Joshua Austill <jlaustill@gmail.com>
2018-05-19 18:14:15 +0000

    update lcd in thread

--- pmac.py
+++ pmac.py
@@ -4,16 +4,23 @@ from w1thermsensor import W1ThermSensor
 from time import sleep
 import RPi.GPIO as GPIO
 import I2C_LCD_driver
+import json
+import threading
+
+config_file = 'config.json'
+outsideTemp = 0
+insideTemp = 0
+
+with open(config_file) as cf:
+    config = json.load(cf)
 
 lcd = I2C_LCD_driver.lcd()
-outsideSensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "0317042d7bff")
-insideSensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "0317207671ff")
+outsideSensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, config["outside_sensor_id"])
+insideSensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, config["inside_sensor_id"])
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 GPIO.output(17, GPIO.LOW)
-
-lowTemp = 35.0
 
 def turn_fan_on():
     GPIO.output(17, GPIO.HIGH)
@@ -30,22 +37,27 @@ def update_insideTemp():
     insideTemp = insideSensor.get_temperature(W1ThermSensor.DEGREES_F)
 
 def update_lcd():
-    lcd.lcd_display_string("Outside Temp: %.1f" % outsideTemp, 1)
-    lcd.lcd_display_string("Inside Temp: %.1f" % insideTemp, 2)
-    lcd.lcd_display_string("Desired Temp: %.1f" % lowTemp, 3)
-    lcd.lcd_display_string("Hi Oliver :)", 4)
+    global outsideTemp,insideTemp,config
+    while True:
+        lcd.lcd_display_string("Outside Temp: %.1f" % outsideTemp, 1)
+        lcd.lcd_display_string("Inside Temp: %.1f" % insideTemp, 2)
+        lcd.lcd_display_string("Desired Temp: %.1f" % config["low_temp"], 3)
+        lcd.lcd_display_string("Hi Oliver :)", 4)
+        sleep(1)
+    
+lcd_thread = threading.Thread(target=update_lcd, args=())
+lcd_thread.daemon = True
+lcd_thread.start()
 
 def lower_temp():
-    global lowTemp
-    if lowTemp > 32.1:
-        lowTemp -= 0.1
-    update_lcd()
+    global config
+    if config["low_temp"] > 32.1:
+        config["low_temp"] -= 0.1
 
 def raise_temp():
-    global lowTemp
-    if lowTemp < 100.1:
-        lowTemp += 0.1
-    update_lcd()
+    global config
+    if config["low_temp"] < 100.1:
+        config["low_temp"] += 0.1
 
 buttonDown = Button(4)
 buttonUp = Button(14)
@@ -56,8 +68,7 @@ buttonUp.when_pressed = raise_temp
 while True:
     update_insideTemp()
     update_outsideTemp()
-    update_lcd()
-    if outsideTemp < insideTemp and insideTemp > lowTemp:
+    if outsideTemp < insideTemp and insideTemp > float(config["low_temp"]):
         turn_fan_on()
     else:
         turn_fan_off()
